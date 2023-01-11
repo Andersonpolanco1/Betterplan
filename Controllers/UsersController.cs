1cca03ca99e1f6eebe936f4039b3c6862608b610
Andersonpolanco1 <andersonpolancocontreras@gmail.com>
2023-01-11 00:09:20 -0400

    Adding endpoint GetUserById

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using BetterplanAPI.Data;
+using BetterplanAPI.Data.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BetterplanAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUsersRepository _usersRepository;
+
+        public UsersController(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _usersRepository.GetById(id);
+            return user is null ? NotFound() : Ok(user);
+        }
+    }
+}
